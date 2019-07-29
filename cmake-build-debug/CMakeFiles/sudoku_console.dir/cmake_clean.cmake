98bdc92b88da3b15c7664bc8cfa8bd20860f6665
chenmy1994 <chenmoryosef@mail.tau.ac.il>
2019-07-29 13:44:58 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/sudoku_console.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku_console.dir/main.c.obj"
+  "sudoku_console.pdb"
+  "sudoku_console.exe"
+  "sudoku_console.exe.manifest"
+  "libsudoku_console.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sudoku_console.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
